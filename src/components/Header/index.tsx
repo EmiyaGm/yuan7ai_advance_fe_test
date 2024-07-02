db8d1ce04f8b3c488c811c23221409fd3fa53e3f
EmiyaGm <464723943@qq.com>
2024-07-03 01:52:01 +0800

    fix: page init

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export function Header() {
+  return (
+    <main className="h-[80px]">
+      <div className="flex items-center">
+        <div>元七AI</div>
+        <div>数码印花文件生成工具-免费版</div>
+      </div>
+    </main>
+  )
+}
