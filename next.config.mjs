7b0c62139ea59da5c5bc44a2d246b92861fa3eac
EmiyaGm <464723943@qq.com>
2024-07-13 19:55:07 +0800

    fix: images domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', '8.130.111.74']
+        domains: ['localhost', '8.130.111.74', 'free.yuanqiai.xyz:8089'],
     }
 };
 
