36504ac2211b938f97d73810dee4a80fab90615f
lightZ <zhangliang51623@163.com>
2024-09-05 07:19:14 +0800

    fix url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,3 @@
 // export const baseUrl = 'http://39.101.77.156:5000'
-// export const baseUrl = 'http://127.0.0.1:8089'
-export const baseUrl = 'http://free.yuanqiai.xyz'
+//export const baseUrl = 'http://127.0.0.1:5000'
+export const baseUrl = 'https://free.yuanqiai.xyz'
