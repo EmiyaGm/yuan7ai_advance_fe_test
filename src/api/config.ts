2b06a1716799e5bd1965d11bd504726c2e4f4642
EmiyaGm <464723943@qq.com>
2025-03-25 00:34:01 +0800

    fix: request url

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,4 +2,4 @@
 //export const baseUrl = 'http://127.0.0.1:5000'
 export const baseUrl = 'https://free.yuanqiai.xyz'
 // export const baseAccountUrl = 'https://mng.yuanqiai.xyz'
-export const baseAccountUrl = 'https://api-test.yuanqiai.xyz/management'
+export const baseAccountUrl = 'https://api.yuanqiai.xyz/management'
